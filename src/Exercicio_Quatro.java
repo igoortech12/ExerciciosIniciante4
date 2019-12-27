d3a04b31f2eff3bd631cdfdfa5eb0f2d6edcd4b1
Igor Oliveira <igoortech@gmail.com>
2019-12-27 18:51:37 -0300

    Exercicio4 iniciado.

---
+++ src/Exercicio_Quatro.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Exercicio_Quatro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int x;
+		int y;
+
+		for (int i = 0; i < N; i++) {
+			x = sc.nextInt();
+			y = sc.nextInt();
+			if (x >= 0 || y >= 0) {
+				double div = x / y;
+				System.out.println(div);
+			} else if (y == 0) {
+				System.out.println("Divisão impossível");
+			}
+		}
+
+		sc.close();
+
+	}
+
+}
